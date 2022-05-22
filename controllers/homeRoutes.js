65eff756dfe8154d3ea0e028b455b44e9b27cdc4
Marcus mbp <maherreraiv1987@gmail.com>
2022-05-22 12:56:19 -0600

    get / route added to homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{ model: User }]
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
