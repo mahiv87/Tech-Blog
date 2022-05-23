ba80f5371e8357db7d13dd281add7781944432ac
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-23 14:54:04 -0600

    UPDATE added to post route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -27,6 +27,30 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.update(
+            {
+                content: req.body.content
+            },
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        );
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post with this id' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.destroy({
