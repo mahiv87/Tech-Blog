6a89cfee85fc045ab3f49e1c9fcc21ea22e1f039
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-23 16:22:55 -0600

    debugged add comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -15,10 +15,29 @@ router.get('/', async (req, res) => {
 router.post('/', withAuth, async (req, res) => {
     try {
         const commentData = await Comment.create({
-            ...req.body,
-            user_id: req.session.user_id,
-            post_id: req.session.post_id
+            comment: req.body.comment,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
         });
+
+        res.status(200).json(commentData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+        }
 
         res.status(200).json(commentData);
     } catch (error) {
