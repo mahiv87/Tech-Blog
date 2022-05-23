e18f63ec8bec4178c4cdbe9c21bae285cf33124d
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-23 11:16:53 -0600

    changed Logout style

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,4 +49,26 @@ router.get('/dashboard', withAuth, async (req, res) => {
     }
 });
 
+// Single post
+router.get('/posts/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name']
+                }
+            ]
+        });
+
+        const post = postData.get({ plain: true });
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
